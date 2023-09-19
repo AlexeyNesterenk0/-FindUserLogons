31d010ca1c4abb5ff73c903fb0045bba59569671
Aleksey Nesterenko <143705665+AlexeyNesterenk0@users.noreply.github.com>
2023-09-19 13:23:08 +0300

    Add files via upload

---
+++ FindUserLogons_CL.ps1
@@ -0,0 +1,88 @@
+﻿<#
+ADFindUserLogon
+Version 3.1
+
+Description: The script outputs data about user logins to the ARM.
+
+Nikita Bratcev 2015
+Anatoliy Krukov 2022
+Aleksey Nesterenko 2023
+
+#>
+$ErrorActionPreference = 'SilentlyContinue'
+ function ConverToHash ($x)
+{
+                $hash = @{}
+
+                
+                $x.psobject.properties | Foreach                 {
+                   
+                    if ($_.Value -match "^\d\d\.\d\d\.\d\d\d\d\s\d{1,}\:\d\d\:\d\d")  # The Date and Time in the blog can have different formats. Matching search by regular expression  
+                    {
+                          $hash[$_.Name] =[datetime]::parseexact($_.Value,"dd.MM.yyyy H:mm:ss",$null) #Parsing the datetime by template #1 into a variable of the datetime type (Required for proper sorting)
+                    }
+                    if ($_.Value -match "^\w\w\w\s\d\d\/\d\d\/\d\d\d\d\/\s\d{1,}\:\d\d\:\d\d") # The Date and Time in the blog can have different formats. Matching search by regular expression
+                    {
+                          $hash[$_.Name] =[datetime]::parseexact($_.Value,"ddd dd/MM/yyyy/ H:mm:ss",$null) #Parsing the datetime by template #2 into a variable of the datetime type (Required for proper sorting)
+                    }
+                  
+                }
+                $hash.getenumerator() | sort-object -property value 
+                Write-Host ("-----`nTotal users: {0}" -f $hash.Count)
+}
+
+    $path="\\AVANGARD.LOC\fs\Bases\scripts\FIFO\Phonebook\logs\dbcache"
+    Clear-Host
+    Write-Host "Collect Users ..."
+
+    Clear-Host
+
+
+    $Users = Get-Content  -RAW $path\Computers.json |  ConvertFrom-Json
+    $Computers = Get-Content -RAW $path\Users.json | ConvertFrom-Json
+
+
+    do
+    {
+       try
+       {
+            Write-Host "--- Для выхода введите Exit или . ---"
+            $choise = Read-Host "Введите имя пользователя или компьютера"
+            if ($choise -eq "exit") {Exit}
+	        if ($choise -eq ".") {Exit}
+
+
+            if (($Users | where name -eq $Choise) -eq $null)
+                {
+   
+                
+                ConverToHash(($Computers | where name -eq $Choise ).Value)           
+
+  
+
+                }
+            else
+  
+            {
+
+            if ($Computers."$Choise" -eq $null)
+                {
+  
+                ConverToHash(($Users | where name -eq $Choise).Value) 
+         
+                }
+            }
+            Write-Host ""
+        }
+        catch
+        {
+			Write-Host "----------------------------------------------------------"
+            Write-Host ("Объект  с именем {0} не обнаружен в AD" -f $Choise) 
+			Write-Host "----------------------------------------------------------"
+			Write-Host "----------------------------------------------------------"
+        }
+    }
+    while ($true)
+
+
+
---
+++ FindUserLogons_SRV.ps1
@@ -0,0 +1,67 @@
+﻿<#
+ADFindUserLogon
+Version 3.1
+
+Description: The script outputs data about user logins to the ARM.
+
+Nikita Bratcev 2015
+Anatoliy Krukov 2022
+Aleksey Nesterenko 2023
+
+#>
+
+$path="\\AVANGARD.LOC\fs\Bases\scripts\FIFO\Phonebook\logs\dbcache"
+Clear-Host
+Write-Host "Collect logs ..."
+$Logs = Get-ChildItem -Path "\\avangard.loc\fs\Bases\Scripts\FIFO\Phonebook\logs\*"
+Clear-Host
+$cnt = $Logs.Count
+[int32]$i = 1
+
+$Computers = @{}
+$Users = @{}
+
+
+ForEach ($Log in $Logs)
+{
+    $data = Import-Csv -Path $Log.FullName -Delimiter ';' -Header "User", "Date", "Time"
+   
+     $Computers.Item($Log.BaseName)= @{}
+    
+    
+    ForEach ($User in ($data | Group-Object -Property "User" | Sort-Object -Property "Name"))
+    {
+       
+    
+        if(-not $Users.ContainsKey($User.Name))
+        {
+            $Users.Item($User.Name) =  @{}  
+            
+         }
+        
+
+        $Dtt=(($User.Group | Select-Object -Property "Date" -Last 1 | %{$_.Date}).Trim()+' '+($User.Group | Select-Object -Property "Time" -Last 1 | %{$_.Time})-replace ".{3}$").Trim() #collect Date and Time
+
+        #Formation of hash tables
+
+        
+        $Computers[$Log.BaseName].Add($User.Name,$Dtt) #
+        $Users[$User.Name].Add($Log.BaseName,$Dtt) 
+        
+      
+        
+    }
+    Write-Progress -Activity "Create dictionaries " -Status "Progress: " -PercentComplete ($i++/$cnt*100) #ProgressBar
+	
+}
+Write-Host "Start Export base"
+Clear-Host
+Write-Host "Export Computers base ..."
+$Computers.GetEnumerator() |  ConvertTo-JSON -Depth 10 | Out-File $path\Computers.json
+Clear-Host
+Write-Host "Export Users base ..."
+$Users.GetEnumerator() |    ConvertTo-JSON -Depth 10 | Out-File $path\Users.json
+Clear-Host
+Write-Host "End Export base"
+
+
